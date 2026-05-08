fdd837d95cb04a73bc8c688184d4a9d00abbf25b
Azrael-fallen <77fc7sgxb9@privaterelay.appleid.com>
2026-05-07 20:20:11 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -184,7 +184,7 @@ class AbyssalHeraldBot extends Client {
 
     app.get('/', (req, res) => {
       res.status(200).json({ 
-        message: 'TitanBot System Online',
+        message: 'Abyssal Herald System Online',
         version: '2.0.0',
         timestamp: new Date().toISOString()
       });
@@ -356,7 +356,7 @@ class AbyssalHeraldBot extends Client {
 }
 
 try {
-  const bot = new TitanBot();
+  const bot = new AbyssalHeraldBot();
   
   const setupShutdown = () => {
     process.on('SIGTERM', () => bot.shutdown('SIGTERM'));
@@ -380,7 +380,7 @@ try {
   process.exit(1);
 }
 
-export default TitanBot;
+export default AbyssalHeraldBot;
 
 
 
